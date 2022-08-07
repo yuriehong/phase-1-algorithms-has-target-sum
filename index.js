f2c276be7303d51d0f66b2750226d65811ab0edf
+ <+>
2022-08-07 13:58:14 -0400

    commit tree bfb46f9d3eaa04a4871d9988fb6f866bd8165116

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i = 0; i < array.length; i++){
+     let sum = target -array[i];
+     for(let j = i+1; j <array.length; j++){
+      if (array[j] == sum){
+        return true;
+      }
+     }
+  }
+  return false;
+
 }
 
 /* 
   Write the Big O time complexity of your function here
+  nested for loop so it's O(n^2)
 */
 
 /* 
@@ -12,6 +23,7 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+  Checking for every number in the array if any number after it can add up to the target.
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +41,15 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 5, 0, 7], 12));
+
+  console.log("");
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 30));
+
+  console.log("");
 }
 
 module.exports = hasTargetSum;
